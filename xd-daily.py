8a19e5035a6d14c7887895ed1d603d8ca7d6a5d2
Guchen1 <33352608+Guchen1@users.noreply.github.com>
2021-10-27 18:08:54 +0800

    Create xd-daily.py

---
+++ xd-daily.py
@@ -0,0 +1,14 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import json
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',"Referer":"https://xxcapp.xidian.edu.cn/site/ncov/xidiandailyup"}
+data={"sfzx":'1','tw':'1','area':'陕西省 西安市 长安区','city':'西安市','province':'陕西省','address':'陕西省西安市长安区兴隆街道竹园三路西安电子科技大学南校区','geo_api_info':'{"type":"complete","position":{"Q":34.12618733724,"R":108.84139458550402,"lng":108.841395,"lat":34.126187},"location_type":"html5","message":"Get ipLocation failed.Get geolocation success.Convert Success.Get address success.","accuracy":83,"isConverted":true,"status":1,"addressComponent":{"citycode":"029","adcode":"610116","businessAreas":[],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"雷甘路","streetNumber":"238号","country":"中国","province":"陕西省","city":"西安市","district":"长安区","township":"兴隆街道"},"formattedAddress":"陕西省西安市长安区兴隆街道竹园三路西安电子科技大学南校区","roads":[],"crosses":[],"pois":[],"info":"SUCCESS"}','sfcyglq':'0','sfyzz':'0','qtqk':'','ymtys':'0'}
+username="20009200044"
+password="Zz13773606509"
+url="https://xxcapp.xidian.edu.cn/uc/wap/login/check"
+p1=requests.Session()
+p2=p1.post(url,headers = headers,data={"username":username,"password":password})
+p2=p1.post("https://xxcapp.xidian.edu.cn/xisuncov/wap/open-report/save",headers=headers,data=data)
+a=json.loads(p2.text)
+print(a['m'])
